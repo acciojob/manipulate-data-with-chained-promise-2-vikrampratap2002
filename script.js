6f9c1985a368474a02dc756aa1f2829d9b932f69
Vikram pratap <122038154+vikrampratap2002@users.noreply.github.com>
2026-04-14 11:52:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+let outputDiv = document.getElementById("output");
+
+function manipulateData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000);
+  });
+}
+
+manipulateData()
+  .then((arr) => {
+    let evenNumbers = arr.filter(num => num % 2 === 0);
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        outputDiv.textContent = evenNumbers.toString(); // "2,4"
+        resolve(evenNumbers);
+      }, 1000);
+    });
+  })
+  .then((evenNumbers) => {
+    let doubled = evenNumbers.map(num => num * 2);
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        outputDiv.textContent = doubled.toString(); // "4,8"
+        resolve(doubled);
+      }, 2000);
+    });
+  });
